646aa3731147912367891b53cb6ec0917e1b4088
Ciremy <ciremy@MacBook-Air-de-Ciremy.local>
2023-04-24 09:29:02 +0200

    setup user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -48,4 +48,43 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { first_name, last_name, email, password, driver_license, age, sex } =
+      req.body;
+    const user = await prisma.user.update({
+      where: {
+        user_id: String(id),
+      },
+      data: {
+        first_name,
+        last_name,
+        email,
+        password,
+        driver_license,
+        age,
+        sex,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.delete({
+      where: {
+        user_id: String(id),
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 export default router;
