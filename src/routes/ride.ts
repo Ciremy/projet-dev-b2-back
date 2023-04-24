fce94580f6cb3469f59665b0a40c395a7d89dea0
Ciremy <ciremy@MacBook-Air-de-Ciremy.local>
2023-04-24 15:23:34 +0200

    basic ride

--- src/routes/ride.ts
+++ src/routes/ride.ts
@@ -80,7 +80,40 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
+/* wait for other routes
+router.put("/takeSeat/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const ride = await prisma.ride.findUnique({
+      where: {
+        ride_id: String(id),
+      },
+    });
+    // check if there are seats available
+    if (ride) {
+      let seats_available: number = ride.seats_available;
+      if (seats_available >= 0) {
+        seats_available = ride.seats_available - 1;
+      } else {
+        res.status(400).json({ message: "No seats available" });
+      }
+      const updatedRide = await prisma.ride.update({
+        where: {
+          ride_id: String(id),
+        },
+        data: {
+          seats_available,
+        },
+      });
+      res.status(200).json(updatedRide);
+    } else {
+      res.status(404).json({ message: "Ride not found" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+*/
 router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
