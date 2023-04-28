950f23d36ee4327351bbc3044e541b34cf31b9b1
Ciremy <ciremy@MacBook-Air-de-Ciremy.local>
2023-04-28 12:41:34 +0200

    comment

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import user from "./routes/user";
 import ride from "./routes/ride";
 import ridePassager from "./routes/ridePassager";
+import conversation from "./routes/conversation";
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(cors());
 app.use("/ridePassager", ridePassager);
 app.use("/user", user);
 app.use("/ride", ride);
+app.use("/conversation", conversation);
 
 export default app;
