2a9816def3693e7bb56c933ae794da100d6395b5
Ciremy <ciremy@MacBook-Air-de-Ciremy.local>
2023-05-02 15:31:03 +0200

    messages routes

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import user from "./routes/user";
 import ride from "./routes/ride";
 import ridePassager from "./routes/ridePassager";
 import conversation from "./routes/conversation";
+import messages from "./routes/messages";
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use("/ridePassager", ridePassager);
 app.use("/user", user);
 app.use("/ride", ride);
 app.use("/conversation", conversation);
+app.use("/messages", messages);
 
 export default app;
